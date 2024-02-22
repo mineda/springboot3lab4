64b3c4cebeb71d8c9671350f64ef449d57722c3a
mineda <mineda@gmail.com>
2024-02-21 21:19:50 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springboot3lab4/Springboot3lab4Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3lab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springboot3lab4/Springboot3lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
