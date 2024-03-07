0bfc42c5e02da89b5fdf4c7d59e65245131ad3db
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-03-07 01:23:53 +0000

    Tratamento de autorizações em UsuarioService

--- src/main/java/br/gov/sp/fatec/springboot3lab4/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springboot3lab4/service/UsuarioService.java
@@ -1,14 +1,18 @@
 package br.gov.sp.fatec.springboot3lab4.service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import br.gov.sp.fatec.springboot3lab4.entity.Autorizacao;
 import br.gov.sp.fatec.springboot3lab4.entity.Usuario;
+import br.gov.sp.fatec.springboot3lab4.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springboot3lab4.repository.UsuarioRepository;
 
 @Service
@@ -16,7 +20,10 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepo;
-        
+
+    @Autowired
+    private AutorizacaoRepository autRepo;
+
     public List<Usuario> buscarTodos() {
         return usuarioRepo.findAll();
     }
@@ -29,7 +36,24 @@ public class UsuarioService {
                 usuario.getSenha().isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados de usuário inválidos!");
         }
+        if(usuario.getAutorizacoes() != null &&
+                !usuario.getAutorizacoes().isEmpty()) {
+            Set<Autorizacao> autorizacoes = 
+                new HashSet<Autorizacao>();
+            for(Autorizacao aut: usuario.getAutorizacoes()) {
+                autorizacoes.add(buscarAutorizacaoPorId(aut.getId()));
+            }
+            usuario.setAutorizacoes(autorizacoes);
+        }
         return usuarioRepo.save(usuario);
+    }
+
+    public Autorizacao buscarAutorizacaoPorId(Long id) {
+        Optional<Autorizacao> autOp = autRepo.findById(id);
+        if(autOp.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Autorização não encontrada!");
+        }
+        return autOp.get();
     }
 
     public Usuario buscarPorId(Long id) {
